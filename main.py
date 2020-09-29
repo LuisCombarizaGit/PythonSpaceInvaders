1b190d5af0cd122636c197b08f24ece084ebea82
Luis Combariza <lcc340@usask.ca>
2020-09-29 14:49:18 -0700

    Finished code to start game window with background and player information

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
+pygame.font.init()
 import os
 import time
 import random
+
+WIDTH, HEIGHT = 750,750
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Space Invaders")
+
 
 # Load Enemy graphics
 RED_SPACE_SHIP = pygame.image.load(os.path.join("pixel_ship_red_small.png"))
@@ -18,6 +24,34 @@ BLUE_LASER = pygame.image.load(os.path.join("pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("pixel_laser_yellow.png"))
 
 # Background
-BG = pygame.image.load(os.path.join("background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("background-black.png")), (WIDTH, HEIGHT))
 
 
+
+
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans",50)
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+        # draw text
+        lives_label = main_font.render(f"Lives: {level}", 1, (255,100,0))
+        level_label = main_font.render(f"Level: {level}", 1, (0,0,0))
+
+        WIN.blit(lives_label,(10,10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)  # Clock tick will be se to 60FPS
+        redraw_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run == False
+
+main()
